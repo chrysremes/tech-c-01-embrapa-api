cff7103314d02b1590bd5b27c7e44393d4f6bad0
Chrystian Remes <chrystian@windowslive.com>
2024-03-30 15:11:40 -0300

    Added_Enum_For_Returns

--- src/main.py
+++ src/main.py
@@ -17,41 +17,25 @@ app = FastAPI()
 def root():
     return {"main":"page"}
 
-@app.get('/get_url/')
-def read_data(option_id:str, suboption_id:str | None = None):
+@app.get('/get/return_type={return_type}')
+def read_data(return_type:DataTypeReturn, option_id:str, suboption_id:str | None = None):
     embrapa = EmbrapaURL(option=option_id,suboption=suboption_id)
     embrapa.request_and_save_to_df()
-    return {"url" : embrapa.url_request}
-
-@app.get('/get_df_as_str/')
-def read_data(option_id:str, suboption_id:str | None = None):
-    embrapa = EmbrapaURL(option=option_id,suboption=suboption_id)
-    embrapa.request_and_save_to_df()
-    return {"df" : embrapa.df.to_string()}
-
-@app.get('/get_df_as_csv/')
-def read_data(option_id:str, suboption_id:str | None = None):
-    embrapa = EmbrapaURL(option=option_id,suboption=suboption_id)
-    embrapa.request_and_save_to_df()
-    return {"df" : embrapa.df.to_csv()}
-
-@app.get('/get_df_as_dict/')
-def read_data(option_id:str, suboption_id:str | None = None):
-    embrapa = EmbrapaURL(option=option_id,suboption=suboption_id)
-    embrapa.request_and_save_to_df()
-    return {"df" : embrapa.df.to_dict()}
-
-@app.get('/get_df_as_json/')
-def read_data(option_id:str, suboption_id:str | None = None):
-    embrapa = EmbrapaURL(option=option_id,suboption=suboption_id)
-    embrapa.request_and_save_to_df()
-    return {"df" : embrapa.df.to_json()}
-
-@app.get('/get_df_as_html_table/')
-def read_data(option_id:str, suboption_id:str | None = None):
-    embrapa = EmbrapaURL(option=option_id,suboption=suboption_id)
-    embrapa.request_and_save_to_df()
-    return responses.HTMLResponse(content=embrapa.df.to_html(), status_code=200)
+    if return_type is DataTypeReturn.url:
+        return {"url" : embrapa.url_request}
+    elif return_type is DataTypeReturn.df_str:
+        return {"df-str" : embrapa.df.to_string()}
+    elif return_type is DataTypeReturn.df_csv:
+        return {"df-csv" : embrapa.df.to_csv()}
+    elif return_type is DataTypeReturn.df_dict:
+        return {"df-dict" : embrapa.df.to_dict()}
+    elif return_type is DataTypeReturn.df_json:
+        return {"df-json" : embrapa.df.to_json()}
+    elif return_type is DataTypeReturn.df_html_tab:
+        return responses.HTMLResponse(content=embrapa.df.to_html(), status_code=200)
+    else:
+        enums = [enum0.value for enum0 in DataTypeReturn]
+        return {"error": "Incorrect Return Type. Choose an option from {enums}"}
 
 if __name__ == "__main__":
     uvicorn.run(app,host="127.0.0.1",port=8000)
