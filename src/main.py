660aee7e34fe5a95ce4bfdd2b181efbb6cc793c8
Chrystian Remes <chrystian@windowslive.com>
2024-04-12 19:11:08 -0300

    spliting_auth_to_multiple_files

--- src/main.py
+++ src/main.py
@@ -1,19 +1,21 @@
-from datetime import datetime, timedelta, timezone
+from datetime import timedelta
 from typing import Annotated
 
 import uvicorn
 from fastapi import FastAPI, HTTPException, responses, status, Depends
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
-from passlib.context import CryptContext
+
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+
 from pydantic import BaseModel
 from enum import Enum
 
 from EmbrapaURL import EmbrapaURL
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
+from auth import Token, User, authenticate_user, create_access_token, get_current_active_user
+
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 fake_users_db = {
     "johndoe": {
@@ -25,22 +27,6 @@ fake_users_db = {
     }
 }
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-class TokenData(BaseModel):
-    username: str | None = None
-
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
-
-class UserInDB(User):
-    hashed_password: str
-
 class DataTypeReturn(str, Enum):
     url = "url"
     df_str = "df_str"
@@ -49,73 +35,12 @@ class DataTypeReturn(str, Enum):
     df_json = "df_json"
     df_html_tab = "df_html_tab"
 
+
 app = FastAPI()
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-def fake_decode_token(token):
-    return User(
-        username=token+"fakedecoded",
-        email="john@example.com",
-        full_name="John Example" 
-    )
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)],
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
+@app.get('/')
+def root():
+    return {"main":"page"}
 
 @app.post("/token")
 async def login_for_access_token(
@@ -140,16 +65,11 @@ async def read_users_me(
 ):
     return current_user
 
-
 @app.get("/users/me/items/")
 async def read_own_items(
     current_user: Annotated[User, Depends(get_current_active_user)],
 ):
     return [{"item_id": "Foo", "owner": current_user.username}]
-
-@app.get('/')
-def root():
-    return {"main":"page"}
 
 @app.get('/items/')
 async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
