8c5e58d11a84d6ee4374e05b36b81c3373a43c10
Chrystian Remes <chrystian@windowslive.com>
2024-04-24 01:02:35 -0300

    Removed_unsed_path_from_API

--- src/main.py
+++ src/main.py
@@ -29,35 +29,6 @@ def read_raw_data_dict(
     embrapa.request_and_save_to_df()
     return {"df-dict" : embrapa.df.to_dict()}
 
-@app.get('/read_raw_any/return_type={return_type}/year={year}/option={option_id}/suboption={suboption_id}', tags=["Embrapa Raw"])
-def read_raw_data_protected(
-        return_type: DataTypeReturn, 
-        year: Annotated[int, Path(title="The ID of the item to get", ge=EmbrapaPages.START_YEAR, le=EmbrapaPages.LAST_YEAR)], 
-        option_id: DataOption, 
-        suboption_id: DataSubOption,
-        current_user: Annotated[User, Depends(get_current_active_user)]
-    )->dict:
-    if suboption_id is DataSubOption.StrNone:
-        suboption_id=None
-    embrapa = EmbrapaWebScrap(year=year, option=option_id, suboption=suboption_id)
-    embrapa.request_and_save_to_df()
-    if return_type is DataTypeReturn.url:
-        return {"url" : embrapa.url_request}
-    elif return_type is DataTypeReturn.df_str:
-        return {"df-str" : embrapa.df.to_string()}
-    elif return_type is DataTypeReturn.df_csv:
-        return {"df-csv" : embrapa.df.to_csv()}
-    elif return_type is DataTypeReturn.df_dict:
-        return {"df-dict" : embrapa.df.to_dict()}
-    elif return_type is DataTypeReturn.df_json:
-        return {"df-json" : embrapa.df.to_json()}
-    elif return_type is DataTypeReturn.df_html_tab:
-        return responses.HTMLResponse(content=embrapa.df.to_html(), status_code=200)
-    else:
-        enums = [enum0.value for enum0 in DataTypeReturn]
-        return {"error": "Incorrect Return Type. Choose an option from {enums}"}
-
-
 @app.get('/read_clean_data/year={year}/option={option_id}/suboption={suboption_id}', tags=["Embrapa Clean"])
 def read_clean_data(
         year: Annotated[int, Path(title="The ID of the item to get", ge=EmbrapaPages.START_YEAR, le=EmbrapaPages.LAST_YEAR)], 
